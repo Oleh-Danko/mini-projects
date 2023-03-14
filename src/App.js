a8d97056293855acb2b4f912815d8133edcfc541
Oleh <danko.oleh@gmail.com>
2023-03-14 14:05:30 +0200

    add converter

--- src/App.js
+++ src/App.js
@@ -1,41 +1,60 @@
 import {useState, useEffect} from 'react';
+import { Block } from './Block';
 import './index.scss';
-import { Success } from './components/Success';
-import { Users } from './components/Users';
 
 function App() {
-  const [user, setUser] = useState([])
-  const [invites, setInvites] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
-  const [success, setSuccess] = useState(false)
-  const [searchValue, setSearchValue] = useState('')
+  const [fromCurrency, setFromCurrency] = useState('UAH')
+  const [toCurrency, setToCurrency] = useState('USD')
+  const [rates, setRates] = useState([])
+  const [fromValue, setFromValue] = useState(0)
+  const [toValue, setToValue] = useState(0)
 
   useEffect(() => {
-    fetch('https://reqres.in/api/users')
+    fetch('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json')
       .then(res => res.json())
-      .then(res => setUser(res.data))
-      .catch(new Error('error'))
-      .finally(setIsLoading(false))
+      .then(json => setRates([...json]))
+      .catch(err => console.Error(err))
   }, [])
 
-  const onClickInvite = (id) => {
-    if (invites.includes(id)) {
-      setInvites(invites => invites.filter(elem => elem !== id))
-    } else {
-      setInvites(invites => [...invites, id])
-    }
+  const onChangeFromValue = (value) => {
+    const fromRate = fromCurrency === 'UAH' ? 1 : rates.filter(el => el.cc === fromCurrency)[0].rate
+    const toRate = toCurrency === 'UAH' ? 1 : rates.filter(el => el.cc === toCurrency)[0].rate
+    const result = ((value / toRate) * fromRate).toFixed(2)
+
+    setToValue(result)
+    setFromValue(value)
+  }
+
+  const onChangeToValue = (value) => {
+    const fromRate = fromCurrency === 'UAH' ? 1 : rates.filter(el => el.cc === fromCurrency)[0].rate
+    const toRate = toCurrency === 'UAH' ? 1 : rates.filter(el => el.cc === toCurrency)[0].rate
+    const result = ((value / fromRate)* toRate).toFixed(2)
+
+    setToValue(value)
+    setFromValue(result)
+  }
+
+  const onChangeFromCurrency = (value) => {
+    setFromCurrency(value)
+    onChangeFromValue(fromValue)
+  }
+  
+  const onChangeToCurrency = (value) => {
+    setToCurrency(value)
+    // onChangeToValue(toValue)
+    onChangeFromValue(fromValue)
   }
 
   return (
     <div className="App">
-      {success ? <Success count={invites.length}/> : <Users 
-        setSearchValue={setSearchValue}
-        searchValue={searchValue} 
-        items={user} 
-        isLoading={isLoading}
-        onClickInvite={onClickInvite}
-        invites={invites}
-        setSuccess={setSuccess} />}
+      <Block 
+        onChangeValue={onChangeFromValue} 
+        value={fromValue} currency={fromCurrency} 
+        onChangeCurrency={onChangeFromCurrency} />
+      <Block 
+        onChangeValue={onChangeToValue} 
+        value={toValue} currency={toCurrency} 
+        onChangeCurrency={onChangeToCurrency} />
     </div>
   );
 }
